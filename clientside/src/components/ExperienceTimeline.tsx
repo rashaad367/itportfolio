8b664df06a95e938d0e8df77991f199edc7f0ad9
rashaad367 <rashaadjones367@gmail.com>
2024-03-16 23:59:30 -0400

    timeline changes

--- clientside/src/components/ExperienceTimeline.tsx
+++ clientside/src/components/ExperienceTimeline.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import style from "./ExperienceTimeline.css";
 
 
 const ExperienceTimelime: React.FC = () => {
     return (<>
 
-            <hr className="container-lg"/>
+        <hr className="container-lg" />
+        <div className="container-lg flex">
+            <h2 className="mr-64">Harris Teeter</h2>
+            <h2 className="mr-64">YMCA</h2>
+            <h2 className="mr-64">University of North Carolina at Charlotte</h2>
+            <h2 className="mr-64">Reunion Marketing</h2>
+        </div>
 
             </>);
 }
