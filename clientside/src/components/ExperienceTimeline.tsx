8baa36af15d488ba8ed6f7a2df7a3b1d2cbbb23f
rashaad367 <rashaadjones367@gmail.com>
2024-03-17 20:41:47 -0400

    creating experience timeline

--- clientside/src/components/ExperienceTimeline.tsx
+++ clientside/src/components/ExperienceTimeline.tsx
@@ -7,10 +7,34 @@ const ExperienceTimelime: React.FC = () => {
 
         <hr className="container-lg" />
         <div className="container-lg flex">
-            <h2 className="mr-64">Harris Teeter</h2>
-            <h2 className="mr-64">YMCA</h2>
-            <h2 className="mr-64">University of North Carolina at Charlotte</h2>
-            <h2 className="mr-64">Reunion Marketing</h2>
+            <div>
+                <h2>Harris Teeter</h2>
+                <p>2017 - 2019</p>
+                <ul>
+                    <li>* Provided efficient and courteous service to customers through using customer service excellence skills.</li>
+                    <li>* Performed opening and closing procedures, such as counting the cash register, restocking, and cleaning.</li>
+                </ul>
+            </div>
+            <div className="mx-40">
+                <h2>YMCA</h2>
+                <p>2018 - 2019</p>
+            </div>
+            <div className="mx-40">
+                <h2>University of North Carolina at Charlotte</h2>
+                <p>2020 - 2023</p>
+            </div>
+            <div className="mr-16">
+                <h2>Reunion Marketing</h2>
+                <p>2023 - 2024</p>
+                <ul>
+                    <li>* Collaborated with the SEO team and its different departments to build appealing pages for hundreds of clients.</li>
+                    <li>* Assisted in increasing the SEO of our clients so they could generate more leads and have higher conversion rates.</li>
+                </ul>
+            </div>
+            
+            
+            
+           
         </div>
 
             </>);
